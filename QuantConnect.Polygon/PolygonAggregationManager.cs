c2e133507f3f87be5d992ce0d000fbda6bc92705
Jhonathan Abreu <jdabreu25@gmail.com>
2023-10-11 15:09:12 -0400

    Improve aggregation manager implementation

--- QuantConnect.Polygon/PolygonAggregationManager.cs
+++ QuantConnect.Polygon/PolygonAggregationManager.cs
@@ -13,14 +13,10 @@
  * limitations under the License.
 */
 
-using System.Collections.Concurrent;
-
 using QuantConnect.Data;
 using QuantConnect.Data.Consolidators;
 using QuantConnect.Data.Market;
 using QuantConnect.Lean.Engine.DataFeeds;
-using QuantConnect.Lean.Engine.DataFeeds.Enumerators;
-using QuantConnect.Logging;
 
 namespace QuantConnect.Polygon
 {
@@ -30,93 +26,19 @@ namespace QuantConnect.Polygon
     /// </summary>
     public class PolygonAggregationManager : AggregationManager
     {
-        private readonly ConcurrentDictionary<SecurityIdentifier, List<KeyValuePair<SubscriptionDataConfig, ScannableEnumerator<BaseData>>>> _enumerators
-            = new();
-
         /// <summary>
-        /// Add new subscription to current <see cref="IDataAggregator"/> instance
+        /// Gets the consolidator to aggregate data for the given config
         /// </summary>
-        /// <param name="dataConfig">defines the parameters to subscribe to a data feed</param>
-        /// <param name="newDataAvailableHandler">handler to be fired on new data available</param>
-        /// <returns>The new enumerator for this subscription request</returns>
-        public IEnumerator<BaseData> Add(SubscriptionDataConfig dataConfig, EventHandler newDataAvailableHandler)
+        protected override IDataConsolidator GetConsolidator(SubscriptionDataConfig config)
         {
-            if (dataConfig.Type != typeof(TradeBar))
+            // We use the TradeBarConsolidator for TradeBar data and the base implementation for everything else,
+            // given that we are aggregating trade bars (that are already aggregated by Polygon) instead of ticks.
+            if (config.Type != typeof(TradeBar))
             {
-                return base.Add(dataConfig, newDataAvailableHandler);
+                return base.GetConsolidator(config);
             }
 
-            var consolidator = new TradeBarConsolidator(dataConfig.Resolution.ToTimeSpan());
-            var isPeriodBased = dataConfig.Resolution != Resolution.Tick;
-            var enumerator = new ScannableEnumerator<BaseData>(consolidator, dataConfig.ExchangeTimeZone, TimeProvider, newDataAvailableHandler, isPeriodBased);
-
-            _enumerators.AddOrUpdate(
-                dataConfig.Symbol.ID,
-                new List<KeyValuePair<SubscriptionDataConfig, ScannableEnumerator<BaseData>>> { new KeyValuePair<SubscriptionDataConfig, ScannableEnumerator<BaseData>>(dataConfig, enumerator) },
-                (k, v) => { return v.Concat(new[] { new KeyValuePair<SubscriptionDataConfig, ScannableEnumerator<BaseData>>(dataConfig, enumerator) }).ToList(); });
-
-            return enumerator;
-        }
-
-        /// <summary>
-        /// Removes the handler with the specified identifier
-        /// </summary>
-        /// <param name="dataConfig">Subscription data configuration to be removed</param>
-        public bool Remove(SubscriptionDataConfig dataConfig)
-        {
-            if (dataConfig.Type != typeof(TradeBar))
-            {
-                return base.Remove(dataConfig);
-            }
-
-            List<KeyValuePair<SubscriptionDataConfig, ScannableEnumerator<BaseData>>> enumerators;
-            if (_enumerators.TryGetValue(dataConfig.Symbol.ID, out enumerators))
-            {
-                if (enumerators.Count == 1)
-                {
-                    List<KeyValuePair<SubscriptionDataConfig, ScannableEnumerator<BaseData>>> output;
-                    return _enumerators.TryRemove(dataConfig.Symbol.ID, out output);
-                }
-                else
-                {
-                    _enumerators[dataConfig.Symbol.ID] = enumerators.Where(pair => pair.Key != dataConfig).ToList();
-                    return true;
-                }
-            }
-            else
-            {
-                Log.Debug($"PolygonAggregationManager.Update(): IDataConsolidator for symbol ({dataConfig.Symbol.Value}) was not found.");
-                return false;
-            }
-        }
-
-        /// <summary>
-        /// Add new data to aggregator
-        /// </summary>
-        /// <param name="input">The new data</param>
-        public void Update(BaseData input)
-        {
-            try
-            {
-                if (!(input is TradeBar))
-                {
-                    base.Update(input);
-                    return;
-                }
-
-                if (_enumerators.TryGetValue(input.Symbol.ID, out var enumerators))
-                {
-                    for (var i = 0; i < enumerators.Count; i++)
-                    {
-                        var kvp = enumerators[i];
-                        kvp.Value.Update(input);
-                    }
-                }
-            }
-            catch (Exception exception)
-            {
-                Log.Error(exception);
-            }
+            return new TradeBarConsolidator(config.Resolution.ToTimeSpan());
         }
     }
 }
